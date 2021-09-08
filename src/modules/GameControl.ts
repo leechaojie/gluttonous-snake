e4cb3b73027dc11b440609caa4587e6bd2dd00aa
lichaojie <chaojie.lee@outlook.com>
2021-09-08 14:42:33 +0800

    处理蛇的相关逻辑

--- src/modules/GameControl.ts
+++ src/modules/GameControl.ts
@@ -12,7 +12,7 @@ class GameControl {
   private scorePanel: ScorePanel
 
   // 存储移动方向
-  private direction: string = 'ArrowRight'
+  private direction: string = ''
 
   // 游戏是否结束
   private isLive: boolean = true
@@ -20,7 +20,7 @@ class GameControl {
   constructor() {
     this.snake = new Snake()
     this.food = new Food()
-    this.scorePanel = new ScorePanel()
+    this.scorePanel = new ScorePanel(10, 2)
     this.init()
   }
 
@@ -78,8 +78,15 @@ class GameControl {
         break
     }
 
-    this.snake.X = X
-    this.snake.Y = Y
+    this.checkEat(X, Y)
+
+    try {
+      this.snake.X = X
+      this.snake.Y = Y
+    } catch (error) {
+      this.isLive = false
+      alert(`${error}, GAME OVER!`)
+    }
 
     // 开启定时器
     this.isLive && setTimeout(() => {
@@ -87,6 +94,18 @@ class GameControl {
     }, 300 - (this.scorePanel.gameLevel - 1) * 30)
   }
 
+  // 检查蛇是否吃到食物
+  checkEat(X: number, Y: number) {
+    // 判断蛇的坐标是否与食物坐标重合
+    if (X === this.food.X && Y === this.food.Y) {
+      // 食物位置改变
+      this.food.change()
+      // 分数增加
+      this.scorePanel.addScore()
+      // 增加蛇身体
+      this.snake.addBody()
+    }
+  }
 }
 
 export default GameControl
